842831aff9d76ea684a536e0d2e38a562ccb3000
Soham Shee <83713160+soham-shee@users.noreply.github.com>
2024-07-12 03:29:18 +0530

    Update firebase.js

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -6,11 +6,11 @@ import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_API_KEY,
-  authDomain: "chatterbox-3c31e.firebaseapp.com",
-  projectId: "chatterbox-3c31e",
-  storageBucket: "chatterbox-3c31e.appspot.com",
-  messagingSenderId: "328967969894",
-  appId: "1:328967969894:web:8d294c6d8122743e9521a7"
+  authDomain: ,
+  projectId: ,
+  storageBucket: ,
+  messagingSenderId: ,
+  appId: 
 };
 
 
